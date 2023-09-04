ac0ee824bfb19708fbb5b27ffe4e3769ff1b978f
Jules Kuehn <jk@jules.lol>
2023-09-04 19:42:26 -0400

    fix login styling, add index page

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from . import magiclink_views, views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("login/", magiclink_views.EmailLoginView.as_view(), name="email_login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("_ah/warmup", views.warmup, name="warmup"),
+]
