875cedbcdd4efa40e1debdb778b1e3bdfbb3b3aa
Jules Kuehn <jk@jules.lol>
2023-09-04 19:34:57 -0400

    fix errors in template

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,24 +2,4 @@ from django.urls import path
 
 from . import magiclink_views, views
 
-urlpatterns = [
-    path("", views.NewChatView.as_view(), name="index"),
-    path("login/", magiclink_views.EmailLoginView.as_view(), name="email_login"),
-    path("logout/", views.logout_view, name="logout"),
-    path("chat/<int:chat_id>", views.ChatView.as_view(), name="chat"),
-    path("chat/chat_response/<int:chat_id>", views.chat_response, name="chat_response"),
-    path(
-        "chat/delete/<int:chat_id>/<str:current_chat>",
-        views.delete_chat,
-        name="delete_chat",
-    ),
-    path("documents/", views.DocumentsView.as_view(), name="documents"),
-    path("documents/<int:doc_id>", views.DocumentsView.as_view(), name="document"),
-    path("summary/<int:doc_id>", views.summary, name="summary"),
-    path("full_text/<int:doc_id>", views.full_text, name="full_text"),
-    path("embeddings/<int:doc_id>", views.generate_embeddings, name="embeddings"),
-    path("query_embeddings", views.query_embeddings, name="query_embeddings"),
-    path("qa_embeddings", views.qa_embeddings, name="qa_embeddings"),
-    path("chat_settings", views.chat_settings, name="chat_settings"),
-    path("_ah/warmup", views.warmup, name="warmup"),
-]
+urlpatterns = []
